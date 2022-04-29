a46b6774d29deea724a3b865213cf2803ca803bc
devops <devops@marisma.net>
2022-04-29 09:41:56 +0200

    add basic structure

---
+++ src/main/java/ch/constructo/application/Application.java
@@ -0,0 +1,30 @@
+package ch.constructo.application;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@NpmPackage(value = "@fontsource/barlow", version = "4.5.0")
+@Theme(value = "constructo", variant = Lumo.DARK)
+@PWA(name = "Constructo", shortName = "Constructo", offlineResources = {})
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
